ff88a1b847429db5027a7219544205eefdf930d8
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-11-06 00:30:06 +0100

    sc

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
 import Auth from './src/screens/Auth';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/styles/colors';
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: colors.primary
+    },
+  };
+  
   const [auth, setAuth] = useState(undefined)
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       {  auth ? <Text>Zona de Usuario</Text> : <Auth/>}
     </PaperProvider>
   );
